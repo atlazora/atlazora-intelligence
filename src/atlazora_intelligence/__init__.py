ddc0a72d2715f7c12facd3858d5303a840862945
atlazorabusiness <atlazora.business@gmail.com>
2026-09-03 11:53:42 +0200

    feat: establish Python intelligence foundation

---
+++ src/atlazora_intelligence/__init__.py
@@ -0,0 +1,1 @@
+"""Atlazora intelligence package."""
---
+++ tests/test_package.py
@@ -0,0 +1,5 @@
+from atlazora_intelligence import __name__ as package_name
+
+
+def test_package_imports() -> None:
+    assert package_name == "atlazora_intelligence"
